46dbc6ca6264d279e67b0bedec193ab3f53382d3
erictsekw-tech <eric.tsekw@gmail.com>
2026-08-17 13:48:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ st.write("---")
 if st.button("📲 Transmit & Verify HACCP Audit Trail (資料傳輸與流程審計)"):
     with st.spinner("Auditing..."):
         time.sleep(0.4)
-    current_time = (datetime.utcnow() + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S GMT+8")
+    current_time = (datetime.utcnow() + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S GMT+8")
         
     if network_mode == "Online (Cloud-Protected Mode / 雲端連線模式)":
         if not (ccp_s and ccp_b and ccp_a and ccp_r):
